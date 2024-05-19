64125a846aa68ee1ac86bdfa9e1797837e5ba612
Marius Kressin <cooldude2713@protonmail.com>
2024-05-18 19:40:17 -0700

    Fix some devise errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :user, path: '',
+                    path_names: { sign_in: 'login', sign_out: 'logout' }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -31,22 +34,4 @@ Rails.application.routes.draw do
   get '/export/html', to: 'hours#html', layout: 'pdf'
   get '/export/csv', to: 'hours#csv', as: 'export_csv'
   get '/export/pdf', to: 'hours#pdf', as: 'export_pdf'
-
-  # Devise Custom Routes
-  devise_scope :user do
-    get '/sign_in', to: 'devise/sessions#new', as: 'new_user_session'
-    post '/sign_in', to: 'devise/sessions#create', as: 'user_session'
-    delete '/users/sign_out', to: 'devise/sessions#destroy', as: 'destroy_user_session'
-    get '/password/new', to: 'devise/passwords#new', as: 'new_user_password'
-    get '/password/edit', to: 'devise/passwords#edit', as: 'edit_user_password'
-    patch '/account/password', to: 'devise/passwords#update', as: 'user_password'
-    get '/sign_up', to: 'devise/registrations#new', as: 'new_user_registration'
-    get '/account/edit', to: 'devise/registrations#edit', as: 'edit_user_registration'
-    patch '/users', to: 'devise/registrations#update', as: 'user_registration'
-    put '/users/password', to: 'devise/passwords#update'
-    post '/users/password', to: 'devise/passwords#create'
-    put '/users', to: 'devise/registrations#update'
-    delete '/users', to: 'devise/registrations#destroy'
-    post '/users', to: 'devise/registrations#create'
-  end
 end
