4c00b0caa678c2b7cb3914c0eb9403606fd96183
Marius Kressin <cooldude2713@protonmail.com>
2024-06-27 13:10:21 -0700

    Fix some bugs related to deleting sub-users.

--- app/javascript/controllers/dialog_controller.js
+++ app/javascript/controllers/dialog_controller.js
@@ -1,33 +1,48 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-		confirm(event) {
-				event.preventDefault();
-				const currentTarget = event.currentTarget;
+  confirm(event) {
+    event.preventDefault();
+    const currentTarget = event.currentTarget;
 
-				Swal.fire({
-						title: 'Are you sure?',
-						text: 'This action cannot be undone.',
-						icon: 'warning',
-						showCancelButton: true,
-						cancelButtonText: 'No',
-						confirmButtonText: 'Yes',
-				})
-						.then(res => !res.isConfirmed || currentTarget.form.submit());
-		}
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This action cannot be undone.',
+      icon: 'warning',
+      showCancelButton: true,
+      cancelButtonText: 'No',
+      confirmButtonText: 'Yes',
+    })
+      .then(res => !res.isConfirmed || currentTarget.form.submit());
+  }
 
-		topic_confirm(event) {
-				event.preventDefault();
-				const currentTarget = event.currentTarget;
+  topic_confirm(event) {
+    event.preventDefault();
+    const currentTarget = event.currentTarget;
 
-				Swal.fire({
-						title: 'Are you sure?',
-						text: 'This topic and all hours associated with it will be permanently deleted.',
-						icon: 'warning',
-						showCancelButton: true,
-						cancelButtonText: 'No',
-						confirmButtonText: 'Yes',
-				})
-						.then(res => !res.isConfirmed || currentTarget.form.submit());
-		}
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This topic and all hours associated with it will be permanently deleted.',
+      icon: 'warning',
+      showCancelButton: true,
+      cancelButtonText: 'No',
+      confirmButtonText: 'Yes',
+    })
+      .then(res => !res.isConfirmed || currentTarget.form.submit());
+  }
+
+  user_confirm(event) {
+    event.preventDefault();
+    const currentTarget = event.currentTarget;
+
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This user and all hours associated with it will be permanently deleted.',
+      icon: 'warning',
+      showCancelButton: true,
+      cancelButtonText: 'No',
+      confirmButtonText: 'Yes',
+    })
+      .then(res => !res.isConfirmed || currentTarget.form.submit());
+  }
 }
