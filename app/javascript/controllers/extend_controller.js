3c73489916094bcac3f5a3112d85f93cee5db089
Marius Kressin <cooldude2713@protonmail.com>
2024-07-02 09:16:23 -0700

    Add grey background to extended sidebar.

--- app/javascript/controllers/extend_controller.js
+++ app/javascript/controllers/extend_controller.js
@@ -2,7 +2,7 @@ import { Controller } from '@hotwired/stimulus';
 
 
 export default class extends Controller {
-	static targets = ['menu', 'chevron', 'chevContainer'];
+	static targets = ['menu', 'chevron', 'chevContainer', 'back'];
 
 	toggleMenu() {
 		if (this.chevronTarget) this.chevronTarget.classList.toggle('rotate-180');
@@ -14,18 +14,26 @@ export default class extends Controller {
 		window.setTimeout((() => { this.menuTarget.classList.toggle('invisible') }).bind(this), 200);
 	}
 
-        toggleSidebar(){
+	toggleSidebar() {
 		if (this.chevronTarget) this.chevronTarget.classList.toggle('rotate-180');
 		if (this.chevContainerTarget) {
 			this.chevContainerTarget.classList.toggle('translate-x-72');
-                        if (this.chevContainerTarget.classList.contains('translate-x-72')) {
-                                this.chevContainerTarget.style.transform = '';
+			if (this.chevContainerTarget.classList.contains('translate-x-72')) {
+				this.chevContainerTarget.style.transform = '';
 			} else {
 				this.chevContainerTarget.style.transform = `translate(${this.menuTarget.getBoundingClientRect().width}px, 0)`;
-                        }
-                }
+			}
+		}
+		this.backTarget.classList.toggle('opacity-0');
 		this.menuTarget.classList.toggle('-translate-x-72');
-        }
+		if (this.backTarget) {
+			if (this.backTarget.classList.contains('hidden')) {
+				this.backTarget.classList.remove('hidden');
+			} else {
+				window.setTimeout((() => { this.backTarget.classList.add('hidden') }).bind(this), 200);
+            }
+		}
+	}
 
 	closeMenu() {
 		if (this.chevronTarget) this.chevronTarget.classList.remove('rotate-180');
@@ -39,7 +47,11 @@ export default class extends Controller {
 		if (this.chevContainerTarget) {
 			this.chevContainerTarget.classList.add('translate-x-72');
 			this.chevContainerTarget.style.transform = '';
-                }
+		}
+		if (this.backTarget) {
+			this.backTarget.classList.add('opacity-0');
+			window.setTimeout((() => { this.backTarget.classList.add('hidden') }).bind(this), 200);
+		}
 		this.menuTarget.classList.add('-translate-x-72');
 	}
 }
